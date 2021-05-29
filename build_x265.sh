c8b97ddcef4544863d36cb78911a2d1903c8800b
lifegpc <g1710431395@gmail.com>
2021-05-29 17:33:20 +0800

    add x265

---
+++ build_x265.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd x265
+mkdir x265-build
+cd x265-build
+cmake -G "MSYS Makefiles" -DSTATIC_LINK_CRT=ON -DWINXP_SUPPORT=OFF \
+    -DENABLE_CLI=OFF -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 ../source
+make
+make install
