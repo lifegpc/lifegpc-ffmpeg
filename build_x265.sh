ecc598bbc618c0008545d22b6e98cf9179419306
lifegpc <g1710431395@gmail.com>
2021-05-29 18:18:02 +0800

    disable assembly for x265

--- build_x265.sh
+++ build_x265.sh
@@ -4,6 +4,6 @@ mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DSTATIC_LINK_CRT=ON -DWINXP_SUPPORT=OFF \
     -DENABLE_CLI=OFF -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 \
-    -DHIGH_BIT_DEPTH=ON -DMAIN12=ON ../source
+    -DHIGH_BIT_DEPTH=ON -DMAIN12=ON -DENABLE_ASSEMBLY=OFF ../source
 make
 make install
