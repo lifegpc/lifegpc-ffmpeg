cdd8b5eb7c365459e409d111affbd2784a5a46cf
lifegpc <g1710431395@gmail.com>
2021-05-30 07:56:22 +0800

    try to patch x265

--- build_x265.sh
+++ build_x265.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd x265
+patch -p1 -i ../x265.patch
 mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DWINXP_SUPPORT=OFF -DENABLE_CLI=OFF \
     -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -DHIGH_BIT_DEPTH=ON \
-    -DMAIN12=ON -DENABLE_ASSEMBLY=OFF \
-    '-DEXTRA_LINK_FLAGS=-static -static-libgcc' ../source
+    -DMAIN12=ON -DENABLE_ASSEMBLY=OFF -DSTATIC_LINK_CRT=ON ../source
 make
 make install
--- build_x265_x86.sh
+++ build_x265_x86.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd x265
+patch -p1 -i ../x265.patch
 mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DWINXP_SUPPORT=ON -DENABLE_CLI=OFF \
-    -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -DENABLE_ASSEMBLY=OFF \
-    '-DEXTRA_LINK_FLAGS=-static -static-libgcc' ../source
+    -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw32 -DENABLE_ASSEMBLY=OFF \
+    -DSTATIC_LINK_CRT=ON ../source
 make
 make install
