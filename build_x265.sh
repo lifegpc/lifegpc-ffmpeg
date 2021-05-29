173f3330cd35e68e0da4df302f64ffe194bcc80b
lifegpc <g1710431395@gmail.com>
2021-05-29 17:52:53 +0800

    use high bit depth for x265 x64 build

--- build_x265.sh
+++ build_x265.sh
@@ -3,6 +3,7 @@ cd x265
 mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DSTATIC_LINK_CRT=ON -DWINXP_SUPPORT=OFF \
-    -DENABLE_CLI=OFF -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 ../source
+    -DENABLE_CLI=OFF -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 \
+    -DHIGH_BIT_DEPTH=ON -DMAIN12=ON ../source
 make
 make install
