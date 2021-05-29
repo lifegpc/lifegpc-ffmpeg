1b77d786ba29014d08617d64a8d356fac0854397
lifegpc <g1710431395@gmail.com>
2021-05-29 20:21:13 +0800

    use new link options

--- build_x265.sh
+++ build_x265.sh
@@ -5,6 +5,6 @@ cd x265-build
 cmake -G "MSYS Makefiles" -DWINXP_SUPPORT=OFF -DENABLE_CLI=OFF \
     -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -DHIGH_BIT_DEPTH=ON \
     -DMAIN12=ON -DENABLE_ASSEMBLY=OFF \
-    '-DEXTRA_LINK_FLAGS=-static-libgcc -static-libstdc++' ../source
+    '-DEXTRA_LINK_FLAGS=-static -static-libgcc' ../source
 make
 make install
--- build_x265_x86.sh
+++ build_x265_x86.sh
@@ -4,6 +4,6 @@ mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DWINXP_SUPPORT=ON -DENABLE_CLI=OFF \
     -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -DENABLE_ASSEMBLY=OFF \
-    '-DEXTRA_LINK_FLAGS=-static-libgcc -static-libstdc++' ../source
+    '-DEXTRA_LINK_FLAGS=-static -static-libgcc' ../source
 make
 make install
