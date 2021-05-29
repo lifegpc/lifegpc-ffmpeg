29e6e9f5d9de02f02ca9a7e834bd8a4ff7e526af
lifegpc <g1710431395@gmail.com>
2021-05-30 07:25:17 +0800

    revert fix in ffmpeg configure

--- configure.sh
+++ configure.sh
@@ -17,7 +17,8 @@ FLAGS+=' --enable-libx265'
 FLAGS+=' --enable-libxml2'
 FLAGS+=' --enable-small'
 FLAGS+=' --enable-version3'
+FLAGS+=' --extra-ldflags=-static'
 FLAGS+=' --pkg-config-flags=--static'
 FLAGS+=" --prefix=$loc"
-./configure $FLAGS "--extra-ldflags=-static -static-libgcc"
+./configure $FLAGS
 echo $?
