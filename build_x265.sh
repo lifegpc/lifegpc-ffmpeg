8315f854829762fcc64104f1a880a13fdb8d2be8
lifegpc <g1710431395@gmail.com>
2021-05-29 20:13:27 +0800

    add -static-libstdc++ link options

--- build_x265.sh
+++ build_x265.sh
@@ -4,7 +4,7 @@ mkdir x265-build
 cd x265-build
 cmake -G "MSYS Makefiles" -DWINXP_SUPPORT=OFF -DENABLE_CLI=OFF \
     -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -DHIGH_BIT_DEPTH=ON \
-    -DMAIN12=ON -DENABLE_ASSEMBLY=OFF -DEXTRA_LINK_FLAGS=-static \
-    '-DEXTRA_LIB=-lgcc -lpthread' ../source
+    -DMAIN12=ON -DENABLE_ASSEMBLY=OFF \
+    '-DEXTRA_LINK_FLAGS=-static-libgcc -static-libstdc++' ../source
 make
 make install
