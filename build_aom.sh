b0a3679e64c44eb57e11e3718f10a6addee158fb
lifegpc <g1710431395@gmail.com>
2021-05-29 15:17:50 +0800

    try to fix aom build

--- build_aom.sh
+++ build_aom.sh
@@ -4,5 +4,6 @@ mkdir aom-build
 cd aom-build
 cmake -DCONFIG_GCC=1 -DSTATIC_LINK_JXL=1 -DENABLE_DOCS=OFF \
     -DENABLE_EXAMPLES=OFF -DENABLE_NASM=ON -DENABLE_TESTS=OFF \
-    -DENABLE_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 ..
-cmake --build . --target install
+    -DENABLE_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/mingw64 -G "MSYS Makefiles" ..
+make
+make install
--- build_aom_x86.sh
+++ build_aom_x86.sh
@@ -4,5 +4,6 @@ mkdir aom-build
 cd aom-build
 cmake -DCONFIG_GCC=1 -DSTATIC_LINK_JXL=1 -DENABLE_DOCS=OFF \
     -DENABLE_EXAMPLES=OFF -DENABLE_NASM=ON -DENABLE_TESTS=OFF \
-    -DENABLE_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/mingw32 ..
-cmake --build . --target install
+    -DENABLE_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/mingw32 -G "MSYS Makefiles" ..
+make
+make install
