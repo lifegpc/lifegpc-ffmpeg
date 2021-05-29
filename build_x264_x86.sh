4eaa50c2cc3e9b511f1df11129f6b6a30407ce89
lifegpc <g1710431395@gmail.com>
2021-05-29 13:42:32 +0800

    fix x264 build bug

--- build_x264_x86.sh
+++ build_x264_x86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd x264
-./configure --prefix=/mingw32 --disable-cli --enable-static --extra-ldflags=-static
+./configure --host=mingw32 --prefix=/mingw32 --disable-cli --enable-static --extra-ldflags=-static
 make
 make install
